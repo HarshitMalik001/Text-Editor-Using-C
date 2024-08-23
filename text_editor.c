f012d40bed266a6ba0e6b9b9181cb13ef57b326c
Harshit Malik <harshit1638.be22@chitkara.edu.in>
2024-08-23 17:16:19 +0530

    2nd commit

--- text_editor.c
+++ text_editor.c
@@ -1,152 +1,40 @@
 #include <ctype.h>
-#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
-#define KILO_VERSION "0.0.1"
-#define KILO_TAB_STOP 8
-#define CTRL_KEY(k) ((k) & 0x1f)
-
 struct termios orig_termios;
 
-void die(const char *s) {
-    perror(s);
-    exit(1);
-}
-
 void disableRawMode() {
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
-        die("tcsetattr");
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
 
 void enableRawMode() {
-    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
-    atexit(disableRawMode);
-
-    struct termios raw = orig_termios;
-    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-    raw.c_oflag &= ~(OPOST);
-    raw.c_cflag |= (CS8);
-    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-    raw.c_cc[VMIN] = 0;
-    raw.c_cc[VTIME] = 1;
-
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
+  struct termios raw = orig_termios;
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
-char editorReadKey() {
-    int nread;
-    char c;
-    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
-        if (nread == -1 && errno != EAGAIN) die("read");
-    }
-    return c;
-}
 
-struct editorConfig {
-    int cx, cy;
-    int screenrows;
-    int screencols;
-    char *filename;
-    char statusmsg[80];
-    time_t statusmsg_time;
-};
-
-struct editorConfig E;
-
-int getCursorPosition(int *rows, int *cols) {
-    char buf[32];
-    unsigned int i = 0;
-
-    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
-
-    while (i < sizeof(buf) - 1) {
-        if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
-        if (buf[i] == 'R') break;
-        i++;
-    }
-    buf[i] = '\0';
-
-    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
-    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
-
-    return 0;
-}
-
-int getWindowSize(int *rows, int *cols) {
-    struct winsize ws;
-
-    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
-            return -1;
-        return getCursorPosition(rows, cols);
+int main() {
+  enableRawMode();
+  while (1) {
+    char c = '\0';
+    read(STDIN_FILENO, &c, 1);
+    if (iscntrl(c)) {
+      printf("%d\r\n", c);
     } else {
-        *cols = ws.ws_col;
-        *rows = ws.ws_row;
-        return 0;
+      printf("%d ('%c')\r\n", c, c);
     }
-}
-
-void editorRefreshScreen() {
-    write(STDOUT_FILENO, "\x1b[2J", 4);
-    write(STDOUT_FILENO, "\x1b[H", 3);
-}
-
-void editorMoveCursor(char key) {
-    switch (key) {
-        case 'w':
-            if (E.cy != 0) E.cy--;
-            break;
-        case 's':
-            if (E.cy != E.screenrows - 1) E.cy++;
-            break;
-        case 'a':
-            if (E.cx != 0) E.cx--;
-            break;
-        case 'd':
-            if (E.cx != E.screencols - 1) E.cx++;
-            break;
-    }
-}
-
-void editorProcessKeypress() {
-    char c = editorReadKey();
-
-    switch (c) {
-        case CTRL_KEY('q'):
-            write(STDOUT_FILENO, "\x1b[2J", 4);
-            write(STDOUT_FILENO, "\x1b[H", 3);
-            exit(0);
-            break;
-
-        case 'w':
-        case 's':
-        case 'a':
-        case 'd':
-            editorMoveCursor(c);
-            break;
-    }
-}
-
-void editorInit() {
-    E.cx = 0;
-    E.cy = 0;
-
-    if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
-}
-
-int main(int argc, char *argv[]) {
-    enableRawMode();
-    editorInit();
-
-    while (1) {
-        editorRefreshScreen();
-        editorProcessKeypress();
-    }
-
-    return 0;
+    if (c == 'q') break;
+  }
+  return 0;
 }
